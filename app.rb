e1ee37dea8535c652fb1b249de7962ed810befe3
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-17 14:08:17 -0700

    wrapped response correctlu

--- app.rb
+++ app.rb
@@ -77,13 +77,13 @@ end
 
 post '/incoming/call/gather' do
   response = Twilio::TwiML::Response.new do |r|
-    # if params[:Digits] == "1"
+    if params[:Digits] == "1"
       r.Say 'Dude, why would you pick one?', :voice => 'woman'
-    # else
-    #   r.Say 'Dude, why would you press two?', :voice => 'woman'
-    # end
-
-    content_type :xml
-    response.text
+    else
+      r.Say 'Dude, why would you press two?', :voice => 'woman'
+    end
   end
+
+  content_type :xml
+  response.text
 end
