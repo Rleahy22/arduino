bd05dc63ad66c4247d52f86a15c424087ff7a402
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-18 20:55:03 -0700

    arduino

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
+require 'arduino_firmata'
+require 'morsecode'
 
+enable :sessions
 # Load configuration from system environment variables - see the README for more
 # on these variables.
 TWILIO_ACCOUNT_SID = ENV['TWILIO_ACCOUNT_SID']
@@ -57,29 +60,13 @@ get '/hello' do
 end
 
 get '/incoming/sms' do
-  response = Twilio::TwiML::Response.new do |r|
-    if session[:state] == nil
-      session[:correct] = 0
-      session[:state] = '1'
-      r.Sms "#{session[:state]} What walks on 4 legs in the morning, 2 legs during the day, and 3 legs at night?"
-    elsif session[:state] == '1'
-      if params[:Body].upcase == 'A HUMAN'
-        session[:correct] =+ 1
-      end
-      r.Sms 'What is black and white and red all over?'
-    elsif session[:state] == '2'
-      if params[:Body] == 'A PENGUIN WITH A SUNTAN'
-        session[:correct] =+ 1
-      end
-      r.Sms 'What is the answer to life, the universe, 
-            and everyhting else?'
-    else
-      if params[:Body] == '42'
-        session[:correct] += 1
-      end
-      r.Sms "#{session[:correct]}/3"
-    end 
-  end
+	@text = params[:body]
+	@morse = MorseCode.new(@text).to_mc
+	arduino = ArduinoFirmata.connect
+
+	arduino.digital_write(arduino)
+	erb :arduino
+end
 
   content_type :xml
   response.text
